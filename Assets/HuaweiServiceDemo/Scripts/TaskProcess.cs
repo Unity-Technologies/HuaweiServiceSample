79e142a109162a472e65df9b6412d91c98300396
siyao <sh2435@cornell.edu>
2021-06-29 16:59:11 +0800

    fix game service callback

---
+++ Assets/HuaweiServiceDemo/Scripts/TaskProcess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskProcess : MonoBehaviour
+{
+    public static LinkedList<Action> tasks = new LinkedList<Action>(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (tasks.Count > 0)
+        {
+            var t = tasks.First;
+            tasks.RemoveFirst();
+            t.Value();
+        }
+        
+    }
+}
