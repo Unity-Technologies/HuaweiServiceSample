554f3c08a024fa1cbfa83ec55827f2841f33bd1f
siyao <sh2435@cornell.edu>
2021-12-23 11:22:41 +0800

    update gradle and init

--- Assets/HuaweiService/fundation/HmsClass.cs
+++ Assets/HuaweiService/fundation/HmsClass.cs
@@ -6,6 +6,28 @@ namespace HuaweiService{
     {
         string name{get;}
     }
+
+    public static class AgcIniter
+    {
+        static bool needInit = true;
+        public static void Init(string name)
+        {
+            if (!needInit)
+            {
+                return;
+            }
+            if (name != null && name.StartsWith("com.huawei.agconnect"))
+            {
+                needInit = false;
+                try
+                {
+                    version.LibraryInfos.getInstance().registerLibraryType("Unity");
+                }catch (System.Exception _)
+                {
+                }
+            }
+        }
+    }
     
     public abstract class HmsClass<T> : IHmsBase where T:IHmsBaseClass, new(){
         private AndroidJavaObject _obj;
@@ -24,6 +46,7 @@ namespace HuaweiService{
         }
         public AndroidJavaObject obj{
             get{
+                AgcIniter.Init(name);
                 return _obj == null?_obj = new AndroidJavaObject(name,HmsUtil.TransferParams(construcArgs)):_obj;
             }
             set{
@@ -40,6 +63,7 @@ namespace HuaweiService{
         public static AndroidJavaClass _clz;
         public static AndroidJavaClass clz{
             get{
+                AgcIniter.Init(name);
                 return _clz == null?_clz = new AndroidJavaClass(name):_clz;
             } 
         }
